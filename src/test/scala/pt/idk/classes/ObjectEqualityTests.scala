b8db7f57c79ed038f51cc16d67c7a38cab2cc5b3
Emanuel Couto <unit73e@gmail.com>
2015-07-29 01:32:49 +0100

    Add object reference equality test

--- src/test/scala/pt/idk/classes/ObjectEqualityTests.scala
+++ src/test/scala/pt/idk/classes/ObjectEqualityTests.scala
@@ -25,4 +25,32 @@ class EqualsTests extends FlatSpec with Matchers {
     assert(p1 == p2)
   }
 
+  "The 'eq' and 'ne' operators" should "test reference equality" in {
+    val s1 = new Point(1, 0)
+    val s2 = s1
+    val s3 = new Point(1, 0)
+
+    /*
+     * The 'a eq b' returns true if 'a' and 'b' reference the same object.
+     *
+     * The variable 's2' should reference the same object as 's1' because 's2'
+     * was defined to be equal to 's1'.
+     */
+    assert(s1 eq s2)
+
+    /*
+     * The 'a ne b' returns true if 'a' and 'b' do not reference the same
+     * object.
+     *
+     * The variables 's1' and 's3' should not reference the same object because
+     * 'new' should always allocate memory for a new object.
+     *
+     * Theoretically, since 'Point' is immutable, it would make sense for 's3'
+     * to just reference the same value as 's1' instead of allocating more
+     * memory. However this has a cost and the JVM has such behavior for some
+     * objects, such as, literal strings (known as string interning).
+     */
+    assert(s1 ne s3)
+  }
+
 }
